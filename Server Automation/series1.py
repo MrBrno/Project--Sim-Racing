a329134f6993a12176f694f1703870fbbfcdc374
Spiers777 <isaacspiers7@gmail.com>
2022-02-14 13:30:23 +0000

    Started to rewrite server automation code

--- Server Automation/series1.py
+++ Server Automation/series1.py
@@ -7,15 +7,49 @@ def getParameters():
 	#get raceMinute from website database
 	#get track from website database
 	#get car from website database
-	#conn.close()
+	conn.close()
+
+def writeToServer(car, track):
+	with open("../servers/Series1/Server1/cfg/server_cfg.ini", "w") as f:
+		f.writelines("") #Clear the document
+	with open("../servers/Series1/Server1/cfg/enty_list.ini", "w") as f:
+			f.writelines("") #Clear the document
+	with open("../servers/Series1/Server1/cfg/server_cfg.ini", "w") as f:
+		f.writelines("") #Clear the document
+	with open("../servers/Series1/Server1/cfg/enty_list.ini", "w") as f:
+			f.writelines("") #Clear the document
+
+	with open("../servers/Series1/Server1/cfg/enty_list.ini", "a") as f:
+		for i in range(0, 19):
+						f.writelines("[CAR_" + str(i) + "]\n")
+						f.writelines("MODEL=" + car + "\n")
+						f.writelines("SKIN=" + "\n")
+						f.writelines("SPECTATOR_MODE=0" + "\n")
+						f.writelines("DRIVERNAME=" + "\n")
+						f.writelines("TEAM=" + "\n")
+						f.writelines("GUID=" + "\n")
+						f.writelines("Ballast=" + "\n")
+						f.writelines("\n")
+
+	with open("../servers/Series1/Server2/cfg/enty_list.ini", "a") as f:
+		for i in range(0, 19):
+						f.writelines("[CAR_" + str(i) + "]\n")
+						f.writelines("MODEL=" + car + "\n")
+						f.writelines("SKIN=" + "\n")
+						f.writelines("SPECTATOR_MODE=0" + "\n")
+						f.writelines("DRIVERNAME=" + "\n")
+						f.writelines("TEAM=" + "\n")
+						f.writelines("GUID=" + "\n")
+						f.writelines("Ballast=" + "\n")
+						f.writelines("\n")
 
 #def startServer():
 	#if minute = raceMinute-1:
-		#subprocess.run(['server1/acServer.exe'])
-		#subprocess.run(['server2/acServer.exe'])
+		#subprocess.run(['../servers/Series1/Server1/acServer.exe'])
+		#subprocess.run(['../servers/Series1/Server2/acServer.exe'])
 
 while True:
 	getParameters()
-	print()
+	writeToServer(car, )
 	#startServer()
 	time.sleep(5)
