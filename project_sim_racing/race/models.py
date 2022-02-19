bfb9a09236ededc023e2f6a69e8a774c232a1c85
Armen._.G <52843550+ArmenG888@users.noreply.github.com>
2022-02-19 14:55:35 -0800

    Added Images for every track

--- project_sim_racing/race/models.py
+++ project_sim_racing/race/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 from django.urls import reverse
 class track(models.Model):
     track_name = models.CharField(max_length=50,default='')
-
+    track_image = models.ImageField(upload_to='track_images',blank=True)
     def __str__(self):
         return self.track_name
     
@@ -12,6 +12,7 @@ class track(models.Model):
 class car(models.Model):
     brand_name = models.CharField(max_length=50,default='')
     model_name = models.CharField(max_length=50,default='')
+    car_image = models.ImageField(upload_to='car_images',blank=True)
     in_game_name = models.CharField(max_length=50,default='ks_')
     def __str__(self):
         return self.brand_name + " " + self.model_name
@@ -21,6 +22,7 @@ class race(models.Model):
     name = models.CharField(max_length=50,default='')
     track = models.ForeignKey(track, on_delete=models.CASCADE)
     car = models.ForeignKey(car, on_delete=models.CASCADE)
+    info = models.TextField(max_length=500,default='')
     minute = models.IntegerField()
     practice_time = models.IntegerField(default=5)
     quaulify_time = models.IntegerField(default=10)
@@ -28,9 +30,8 @@ class race(models.Model):
     server_ip = models.CharField(max_length=20, default="195.211.11.141")
     port_number = models.IntegerField(default=8080)
 
+
     def time_left(self):
         return self.time - timezone.now()
     def __str__(self):
         return self.name
-    def get_absolute_url(self):
-        return reverse('race-detail', kwargs={'pk': self.pk})
