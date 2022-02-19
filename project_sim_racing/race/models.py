ea9166b5f544bdcb9910805ec89998892aea4032
Armen._.G <52843550+ArmenG888@users.noreply.github.com>
2022-02-19 09:01:04 -0800

    bug fix

--- project_sim_racing/race/models.py
+++ project_sim_racing/race/models.py
@@ -16,24 +16,7 @@ class car(models.Model):
     def __str__(self):
         return self.brand_name + " " + self.model_name
 
-class server1(models.Model):
-    practice_server = models.BooleanField(default=False)
-    name = models.CharField(max_length=50,default='')
-    track = models.ForeignKey(track, on_delete=models.CASCADE)
-    car = models.ForeignKey(car, on_delete=models.CASCADE)
-    minute = models.IntegerField()
-    practice_time = models.IntegerField(default=5)
-    qualify_time = models.IntegerField(default=10)
-    race_laps = models.IntegerField(default=5)
-    server_ip = models.CharField(max_length=20, default="195.211.11.141")
-    port_number = models.IntegerField(default=8080)
-
-    def time_left(self):
-        return self.time - timezone.now()
-    def __str__(self):
-        return self.name
-
-class server2(models.Model):
+class race(models.Model):
     practice_server = models.BooleanField(default=False)
     name = models.CharField(max_length=50,default='')
     track = models.ForeignKey(track, on_delete=models.CASCADE)
@@ -50,36 +33,3 @@ class server2(models.Model):
     def __str__(self):
         return self.name
 
-class server3(models.Model):
-    practice_server = models.BooleanField(default=False)
-    name = models.CharField(max_length=50,default='')
-    track = models.ForeignKey(track, on_delete=models.CASCADE)
-    car = models.ForeignKey(car, on_delete=models.CASCADE)
-    minute = models.IntegerField()
-    practice_time = models.IntegerField(default=5)
-    quaulify_time = models.IntegerField(default=10)
-    race_laps = models.IntegerField(default=5)
-    server_ip = models.CharField(max_length=20, default="195.211.11.141")
-    port_number = models.IntegerField(default=8080)
-
-    def time_left(self):
-        return self.time - timezone.now()
-    def __str__(self):
-        return self.name
-
-class server4(models.Model):
-    practice_server = models.BooleanField(default=False)
-    name = models.CharField(max_length=50,default='')
-    track = models.ForeignKey(track, on_delete=models.CASCADE)
-    car = models.ForeignKey(car, on_delete=models.CASCADE)
-    minute = models.IntegerField()
-    practice_time = models.IntegerField(default=5)
-    quaulify_time = models.IntegerField(default=10)
-    race_laps = models.IntegerField(default=5)
-    server_ip = models.CharField(max_length=20, default="195.211.11.141")
-    port_number = models.IntegerField(default=8080)
-
-    def time_left(self):
-        return self.time - timezone.now()
-    def __str__(self):
-        return self.name
--- project_sim_racing/race/views.py
+++ project_sim_racing/race/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
-from .models import server1 as race1
-from .models import server2 as race2
-from .models import server3 as race3
+from .models import race as r
 from datetime import datetime 
 def race_views(request):
     context = {
-        'race1': race1.objects.all()
+        'races': r.objects.all()
     }
     return render(request, 'race/races.html', context)
 
