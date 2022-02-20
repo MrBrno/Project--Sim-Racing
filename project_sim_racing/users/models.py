5849fb3f794d369fd5b3e95a068fa269b27c52ba
Armen._.G <52843550+ArmenG888@users.noreply.github.com>
2022-02-20 08:46:10 -0800

    Safety Rating

--- project_sim_racing/users/models.py
+++ project_sim_racing/users/models.py
@@ -4,7 +4,12 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    #safety = models.IntegerField(default="100")
+    safety_rating = models.IntegerField(default="0")
+    wins = models.IntegerField(default="0")
+    poles = models.IntegerField(default="0")
+    podiums = models.IntegerField(default="0")
+    laps_lead = models.IntegerField(default="0")
+    races_started = models.IntegerField(default="0")
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
     
     def __str__(self):
