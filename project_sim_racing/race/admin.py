8f2c99e7bc8f1f131ac51d5ee67be779fefb86a4
Armen._.G <52843550+ArmenG888@users.noreply.github.com>
2022-02-21 16:57:09 -0800

    Multiple cars for a single race

--- project_sim_racing/race/admin.py
+++ project_sim_racing/race/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import car,track,race
+from .models import car,track,race,cars_for_the_race
 
 admin.site.register(car)
 admin.site.register(track)
 admin.site.register(race)
+admin.site.register(cars_for_the_race)
